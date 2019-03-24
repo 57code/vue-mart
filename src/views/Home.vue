5a1b8b330e4c353507b07127f115e3546371c9d0
yt0379 <yt0379@qq.com>
2019-03-24 13:51:13 +0800

    f

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,14 @@
 <template>
   <div class="home">
-    <!-- 轮播图 -->
-    <cube-slide :data="slider" :interval="5000">
-      <cube-slide-item v-for="(item,index) in slider" :key="index">
-        <router-link :to="`/detail/${item.id}`">
-          <img class="slider" :src="item.img">
-        </router-link>
-      </cube-slide-item>
-    </cube-slide>
-
-    <!-- 商品列表 -->
-    <good-list :data="goods"></good-list>
+    
   </div>
 </template>
 
 <script>
-import {mapState, mapActions, mapGetters} from 'vuex'
-import GoodList from '@/components/GoodList.vue';
+
 
 export default {
-  name: "home",
-  components: {
-    GoodList,
-  },
-  created() {
-    this.getGoods();
-  },
-  computed: {
-    ...mapState(['slider']),
-    ...mapGetters(['goods'])
-  },
-  methods: {
-    ...mapActions(['getGoods'])
-  },
+  name: "home"
 };
 </script>
 style <style lang="stylus" scoped>
